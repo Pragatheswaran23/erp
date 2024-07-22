48de228e37cf762b39162a5e59d078292bda14c2
Pragatheswaran23 <pragatheswaran.sp23@gmail.com>
2024-07-22 07:46:20 +0530

    students first commit

---
+++ student/student-academics.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="keywords" content="">
+
+<title>Academic Details</title>
+
+<link rel="icon" type="image/x-icon" href="../assets/img/logo/favicon.png">
+
+<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="../assets/css/all-fontawesome.min.css">
+<link rel="stylesheet" href="../assets/css/animate.min.css">
+<link rel="stylesheet" href="../assets/css/magnific-popup.min.css">
+<link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="../assets/css/style.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="preloader">
+<div class="loader-book">
+<div class="loader-book-page"></div>
+<div class="loader-book-page"></div>
+<div class="loader-book-page"></div>
+</div>
+</div>
+
+
+<header class="header">
+
+<div class="header-top">
+<div class="container">
+<div class="header-top-wrapper">
+<div class="header-top-left">
+<div class="header-top-contact">
+<ul>
+<li>
+<div class="header-top-contact-icon">
+<i class="far fa-location-dot"></i>
+</div>
+<div class="header-top-contact-info">
+<a href="#">25/B Milford Road, New York</a>
+</div>
+</li>
+<li>
+<div class="header-top-contact-icon">
+<i class="far fa-envelopes"></i>
+</div>
+<div class="header-top-contact-info">
+<a href="/cdn-cgi/l/email-protection#1e777078715e7b667f736e727b307d7173"><span class="__cf_email__" data-cfemail="f1989f979eb19489909c819d94df929e9c">[email&#160;protected]</span></a>
+</div>
+</li>
+<li>
+<div class="header-top-contact-icon">
+<i class="far fa-phone-volume"></i>
+</div>
+<div class="header-top-contact-info">
+<a href="tel:+21236547898">+2 123 654 7898</a>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="header-top-right">
+<div class="header-top-social">
+<span>Follow Us: </span>
+<a href="#"><i class="fab fa-facebook-f"></i></a>
+<a href="#"><i class="fab fa-instagram"></i></a>
+<a href="#"><i class="fab fa-youtube"></i></a>
+<a href="#"><i class="fab fa-whatsapp"></i></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="main-navigation">
+<nav class="navbar navbar-expand-lg">
+<div class="container position-relative">
+<a class="navbar-brand" href="index.html">
+<img src="../assets/img/logo/logo.png" alt="logo">
+</a>
+<div class="mobile-menu-right">
+<div class="search-btn">
+<button type="button" class="nav-right-link search-box-outer"><i class="far fa-search"></i></button>
+</div>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-mobile-icon"><i class="far fa-bars"></i></span>
+</button>
+</div>
+
+<div class="collapse navbar-collapse" id="main_nav">
+<ul class="navbar-nav">
+<li class="nav-item ">
+<a class="nav-link active" href="student-details.php" >Personal</a>
+</li>
+<li class="nav-item ">
+<a class="nav-link active" href="student-academics.php" >Academics</a>
+</li>
+</ul>
+</div>
+</div>
+</nav>
+</div>
+</header>
+
+
+<main class="main">
+
+<div class="application pd-50">
+    <div class="container col-xl-7">
+        <div class="application-form">
+            <h3>Academic Details</h3>
+            <form action="#">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="form-group">
+                            <h6>Name</h6>
+                        </div>
+                    </div>
+                    <div class="col-lg-9">
+                        <div class="form-group">
+                            <label>ANOO R</label>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="form-group">
+                            <h6>Roll No</h6>
+                        </div>
+                    </div>
+                    <div class="col-lg-9">
+                        <div class="form-group">
+                            <label>2020PECAI101</label>
+                        </div>
+                    </div>
+                    <div class="col-lg-12">
+                        <div class="form-group">
+                            <h6>Semester Results</h6>
+                        </div>
+                    </div>
+                    <div class="col-lg-12">
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Semester</th>
+                                    <th scope="col">GPA</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Semester 1</th>
+                                    <td>9.1</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Semester 2</th>
+                                    <td>9.5</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Semester 3</th>
+                                    <td>9.0</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Semester 4</th>
+                                    <td>9.7</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Semester 5</th>
+                                    <td>9.5</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Semester 6</th>
+                                    <td>9.4</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Semester 7</th>
+                                    <td>9.6</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Semester 8</th>
+                                    <td>9.9</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="form-group">
+                            <h6>Total CGPA</h6>
+                        </div>
+                    </div>
+                    <div class="col-lg-9">
+                        <div class="form-group">
+                            <label>9.5</label>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+</main>
+
+<a href="#" id="scroll-top"><i class="far fa-arrow-up-from-arc"></i></a>
+
+
+<script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="../assets/js/jquery-3.7.1.min.js"></script>
+<script src="../assets/js/modernizr.min.js"></script>
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/imagesloaded.pkgd.min.js"></script>
+<script src="../assets/js/jquery.magnific-popup.min.js"></script>
+<script src="../assets/js/isotope.pkgd.min.js"></script>
+<script src="../assets/js/jquery.appear.min.js"></script>
+<script src="../assets/js/jquery.easing.min.js"></script>
+<script src="../assets/js/owl.carousel.min.js"></script>
+<script src="../assets/js/counter-up.js"></script>
+<script src="../assets/js/wow.min.js"></script>
+<script src="../assets/js/main.js"></script>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ student/student-login.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="keywords" content="">
+
+<title>Panimalar Engineering College</title>
+
+<link rel="icon" type="image/x-icon" href="../assets/img/logo/favicon.png">
+
+<link rel="stylesheet" href="../assets/css/bootstrap.min.css">  
+<link rel="stylesheet" href="../assets/css/all-fontawesome.min.css">
+<link rel="stylesheet" href="../assets/css/animate.min.css">
+<link rel="stylesheet" href="../assets/css/magnific-popup.min.css">
+<link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+
+<div class="preloader">
+<div class="loader-book">
+<div class="loader-book-page"></div>
+<div class="loader-book-page"></div>
+<div class="loader-book-page"></div>
+</div>
+</div>
+
+
+<header class="header">
+
+<div class="header-top">
+<div class="container">
+<div class="header-top-wrapper">
+<div class="header-top-left">
+<div class="header-top-contact">
+<ul>
+<li>
+<div class="header-top-contact-icon">
+<i class="far fa-location-dot"></i>
+</div>
+<div class="header-top-contact-info">
+<a href="#">25/B Milford Road, New York</a>
+</div>
+</li>
+<li>
+<div class="header-top-contact-icon">
+<i class="far fa-envelopes"></i>
+</div>
+<div class="header-top-contact-info">
+<a href="/cdn-cgi/l/email-protection#6b02050d042b0e130a061b070e45080406"><span class="__cf_email__" data-cfemail="b1d8dfd7def1d4c9d0dcc1ddd49fd2dedc">[email&#160;protected]</span></a>
+</div>
+</li>
+<li>
+<div class="header-top-contact-icon">
+<i class="far fa-phone-volume"></i>
+</div>
+<div class="header-top-contact-info">
+<a href="tel:+21236547898">+2 123 654 7898</a>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="header-top-right">
+<div class="header-top-social">
+<span>Follow Us: </span>
+<a href="#"><i class="fab fa-facebook-f"></i></a>
+<a href="#"><i class="fab fa-instagram"></i></a>
+<a href="#"><i class="fab fa-youtube"></i></a>
+<a href="#"><i class="fab fa-whatsapp"></i></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</header>
+
+
+<div class="search-popup">
+<button class="close-search"><span class="far fa-times"></span></button>
+<form action="#">
+<div class="form-group">
+<input type="search" name="search-field" placeholder="Search Here..." required="">
+<button type="submit"><i class="far fa-search"></i></button>
+</div>
+</form>
+</div>
+
+<main class="main">
+
+
+
+
+<div class="login-area pd-50">
+<div class="container">
+<div class="col-md-5 mx-auto">
+<div class="login-form">
+<div class="login-header">
+<div class="course-img">
+<img src="../assets/img/logo/logo.png" alt="">
+</div>
+<p>Student Login</p>
+</div>
+<form action="login.php" method="POST">
+<div class="form-group">
+<label>Student Id</label>
+<input type="email" name = "email" class="form-control" placeholder="Your Email">
+</div>
+<div class="form-group">
+<label>Password</label>
+<input type="password" name ="password" class="form-control" placeholder="Your Password">
+</div>
+<div class="d-flex align-items-center">
+<button type="submit" class="theme-btn"><i class="far fa-sign-in"></i> Login</button>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+
+</main>
+
+
+
+<a href="#" id="scroll-top"><i class="far fa-arrow-up-from-arc"></i></a>
+
+
+<script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="../assets/js/jquery-3.7.1.min.js"></script>
+<script src="../assets/js/modernizr.min.js"></script>
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/imagesloaded.pkgd.min.js"></script>
+<script src="../assets/js/jquery.magnific-popup.min.js"></script>
+<script src="../assets/js/isotope.pkgd.min.js"></script>
+<script src="../assets/js/jquery.appear.min.js"></script>
+<script src="../assets/js/jquery.easing.min.js"></script>
+<script src="../assets/js/owl.carousel.min.js"></script>
+<script src="../assets/js/counter-up.js"></script>
+<script src="../assets/js/wow.min.js"></script>
+<script src="../assets/js/main.js"></script>
+</body>
+</html>
